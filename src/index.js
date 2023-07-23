1d0923ed58911569491027189f627831a245282a
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-07-23 06:24:33 +0530

    Added flights model with foreign key constraint defined during model

--- src/index.js
+++ src/index.js
@@ -10,17 +10,9 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, async() => {
+app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    // bad code alert
-    const { City, Airport } = require('./models');
-    // const city = await City.findByPk(9);
-    // await city.createAirport({name: 'Indore airpost', code: 'IND'});
-    await City.destroy({
-        where: {
-            id:9
-        }
-    });
+    
 });
 
 
